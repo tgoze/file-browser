c557b25f70feb2e979814102512aa053d545359d
TJ Goze <tjgoze@gmail.com>
2019-10-31 12:02:33 -0500

    Initial work on passing file names

--- file-browser/Pages/Index.cshtml.cs
+++ file-browser/Pages/Index.cshtml.cs
@@ -6,10 +6,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace file_browser.Pages
-{
+{    
     public class IndexModel : PageModel
     {
         [BindProperty]
-        public string Message { get; set; }        
+        public string[] files { get; set; }
+    
+        public void OnGet()
+        {
+            files = new string[]{"file1", "file2", "file3"};      
+        }
     }
 }
